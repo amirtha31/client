cb84b87eff3d294609760caca790b3c18ec9f16a
amirtha <amirtha.31ten@gmail.com>
2024-03-17 13:56:23 +0530

    admindashboard changed

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -1,15 +1,37 @@
+// import './App.css';
+// import Sidebar from './Sidebar';
+// import Content from './Content';
+// import { useState } from 'react';
+
+// export default function AdminDashboard(){
+//     const [page, setPage] = useState('');
+//     return(
+//         <div>
+//             <Sidebar setPage={setPage} />
+//       <Content page={page} />
+            
+//         </div>
+//     )
+// }
+
 import './App.css';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Content from './Content';
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-export default function AdminDashboard(){
-    const [page, setPage] = useState('');
-    return(
-        <div>
-            <Sidebar setPage={setPage} />
+export default function AdminDashboard() {
+  const [page, setPage] = useState('');
+
+  return (
+    <div>
+      <Sidebar setPage={setPage} />
       <Content page={page} />
-            
-        </div>
-    )
+      {/* Nested Routes for Sidebar and Content */}
+      <Routes>
+        <Route path="sidebar" element={<Sidebar setPage={setPage} />} />
+        <Route path="content" element={<Content page={page} />} />
+      </Routes>
+    </div>
+  );
 }
