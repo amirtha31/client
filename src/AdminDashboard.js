586cca389861c8dc767c72361d010202f210232b
amirtha <amirtha.31ten@gmail.com>
2024-03-17 14:43:43 +0530

    final admindashboard

--- src/AdminDashboard.js
+++ src/AdminDashboard.js
@@ -1,37 +1,16 @@
-// import './App.css';
-// import Sidebar from './Sidebar';
-// import Content from './Content';
-// import { useState } from 'react';
-
-// export default function AdminDashboard(){
-//     const [page, setPage] = useState('');
-//     return(
-//         <div>
-//             <Sidebar setPage={setPage} />
-//       <Content page={page} />
-            
-//         </div>
-//     )
-// }
-
 import './App.css';
-import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Content from './Content';
-import { Routes, Route } from 'react-router-dom';
+// import { useState } from 'react';
 
-export default function AdminDashboard() {
-  const [page, setPage] = useState('');
-
-  return (
-    <div>
-      <Sidebar setPage={setPage} />
-      <Content page={page} />
-      {/* Nested Routes for Sidebar and Content */}
-      <Routes>
-        <Route path="sidebar" element={<Sidebar setPage={setPage} />} />
-        <Route path="content" element={<Content page={page} />} />
-      </Routes>
-    </div>
-  );
+export default function AdminDashboard(){
+    // const [page, setPage] = useState('');
+    return(
+        <div>
+            {/* <Sidebar setPage={setPage} />
+      <Content page={page} /> */}
+            <Sidebar></Sidebar>
+            <Content></Content>
+        </div>
+    )
 }
